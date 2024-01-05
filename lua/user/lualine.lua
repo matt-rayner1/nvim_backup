76a95dc453e029302cd74677ef71a2847600b320
Rayner <Matt.Rayner@Honeywell.com>
2024-01-05 13:33:00 +0000

    small lualine tweak (keep full path on buffer switch)

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -40,7 +40,12 @@ require('lualine').setup {
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
+    lualine_c = {
+        {
+            'filename',
+            path = 3,
+        }
+    },
     lualine_x = {'location'},
     lualine_y = {},
     lualine_z = {}
